daecf241ac1aaa57d701741147dc0fb7a2aa26f6
Евгения Лашко <smevlk2014@gmail.com>
2024-10-14 20:10:26 +0300

    removed token comment

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -11,5 +11,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-  //   eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzI4ODQ4ODg3LCJleHAiOjE3Mjg5MzUyODd9.LmoMpwZtrozkqeQc9nX1tKnp9hF5ik_4dp_gw1ZCgEs
 }
